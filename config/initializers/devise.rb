2a5766115c3d79bca7593f180ce9b3960b7fdc63
Andre Merten <andre.mertn@ibm.com>
2019-06-29 17:07:08 -0700

    production email template

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'andre.merten.CTO@discussions_forum.com'
+  config.mailer_sender = 'andre.merten.CTO@discussions.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
