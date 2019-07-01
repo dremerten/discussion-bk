adf75ad2e0e76c8a962ee2418b7d73a87766e6ab
Andre Merten <andre.mertn@ibm.com>
2019-06-30 22:40:46 -0700

    add staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -86,11 +86,11 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
- config.action_mailer.perform_deliveries = true
+config.action_mailer.perform_deliveries = true
 
-  # Setup the sendgrip mailer config
-  config.action_mailer.default_url_options = { :host => 'https://dremerten-discussions.herokuapp.com/' }
-  #config.action_mailer.default_url_options = { :host => 'https://dre-discussions-staging.herokuapp.com/' }
+# Setup the sendgrip mailer config
+  #config.action_mailer.default_url_options = { :host => 'https://dremerten-discussions.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://dre-discussions-staging.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
