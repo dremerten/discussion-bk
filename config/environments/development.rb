3fb5399e6496d91e689da3e4ff0b9240d986c75f
Andre Merten <andre.mertn@ibm.com>
2019-07-02 15:02:29 -0700

    update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # Host and port of current system
   # For development enviroment we are using gem called mailcatcher, refer to google for more info
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
