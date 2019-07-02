dcddf2af7aa2357d24e799fcc215783bbe54e4e1
Andre Merten <andre.mertn@ibm.com>
2019-07-02 14:48:38 -0700

    update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              '127.0.0.1',
+    address:              'localhost',
     port:                 1025,
   }
 end
