5addcabf6fbf56501db698dc1c4ae7107a5affdb
Andre Merten <andre.mertn@ibm.com>
2019-07-03 11:31:35 -0700

    allow blank true discussion model

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,8 +3,8 @@ class Discussion < ApplicationRecord
   belongs_to :user
   has_many :replies, dependent: :destroy  # if a discussion is deleted, so are the replies
   #before_validation :check_for_existence
-  validates :title, presence: true
-  validates :content, presence: true
+  validates :title, presence: true, allow_blank: false
+  validates :content, presence: true , allow_blank: false
   validates :channel_id, presence: true, allow_blank: false # checks that all fields are filled in, in order to submit to a discussion
   resourcify
 
