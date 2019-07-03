aa7578537ab9f80c36fba1118f79b4bf8f9a4117
Andre Merten <andre.mertn@ibm.com>
2019-07-03 11:47:13 -0700

    presence true

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,9 +3,9 @@ class Discussion < ApplicationRecord
   belongs_to :user
   has_many :replies, dependent: :destroy  # if a discussion is deleted, so are the replies
   #before_validation :check_for_existence
-  validates :title, allow_blank: false, length: { minimum: 1, maximum: 50 }
-  validates :content, allow_blank: false, length: { minimum: 1, maximum: 500 }
-  validates :channel_id, allow_blank: false # checks that all fields are filled in, in order to submit to a discussion
+  validates :title, presence: true, length: { minimum: 1, maximum: 50 }
+  validates :content, presence: true, length: { minimum: 1, maximum: 500 }
+  validates :channel_id, presence: true  # checks that all fields are filled in, in order to submit to a discussion
   resourcify
 
   extend FriendlyId
